7a34ad2253a9f610d9f582e50f08002c3cdc0134
Bryan C Guner <bryan.guner@gmail.com>
2023-05-17 19:32:41 -0400

    used setInterval, useState and useEffect to update time to the second in Demo.js

--- 1-intro/src/components/Demo.js
+++ 1-intro/src/components/Demo.js
@@ -1,14 +1,26 @@
-
-
+import classes from "./Demo.module.css";
+import { useState, useEffect } from "react";
+import { getCurrentDateTime } from "../helpers/date-time";
 const Demo = () => {
-    
-    
-    return (
-        <div>
-            <h1>Hi, I'm a component</h1>
-            <p>My name is Demo</p>
-            <small>Copyright Bryan Guner</small>
-        </div>
-    )
-}
+  const [date, setDate] = useState(getCurrentDateTime());
+  useEffect(() => {
+    setInterval(() => {
+      let update = getCurrentDateTime();
+      setDate(update);
+    }, 1000);
+  }, []);
+  return (
+    <div>
+      <h1 className={classes.special}>Hi, I'm a component</h1>
+      <p>
+        JSX can only have one top level html element... you can use a fragment
+        to wrap otherwise adjacent elements
+      </p>
+      <p className={classes.bold}>
+        The current date & time is <span className={classes.date}>{date}</span>
+      </p>
+      <small>Copyright Bryan Guner</small>
+    </div>
+  );
+};
 export default Demo;
---
+++ 1-intro/src/helpers/date-time.js
@@ -0,0 +1,27 @@
+const addLeadingZero = (value) => {
+  return value < 10 ? "0" + value : value;
+};
+
+export const getCurrentDateTime = () => {
+  const currentDate = new Date();
+
+  const currentDay = addLeadingZero(currentDate.getDate());
+  const currentMonth = addLeadingZero(currentDate.getMonth() + 1); // Months are zero-based
+  const currentYear = currentDate.getFullYear();
+  let currentHours = currentDate.getHours();
+    const currentMinutes = addLeadingZero( currentDate.getMinutes() );
+    const currentSeconds = addLeadingZero( currentDate.getSeconds() );
+  const amOrPm = currentHours >= 12 ? "PM" : "AM";
+
+  // Convert hours to standard time
+  if (currentHours > 12) {
+    currentHours -= 12;
+  } else if (currentHours === 0) {
+    currentHours = 12;
+  }
+
+  const dateString = `${currentDay}/${currentMonth}/${currentYear}`;
+  const timeString = `${currentHours}:${currentMinutes}:${currentSeconds} ${amOrPm}`;
+
+  return `${dateString} ${timeString}`;
+};
