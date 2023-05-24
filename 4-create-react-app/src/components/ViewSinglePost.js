050370109c856d83fb72501017e5c5c628caa1ad
Bryan C Guner <bryan.guner@gmail.com>
2023-05-24 14:30:04 -0400

    populated most of the hardcoded values in ViewSinglePost with post state and changed the useEffect in Page.js to have a dependency of props.title

--- 4-create-react-app/src/components/ViewSinglePost.js
+++ 4-create-react-app/src/components/ViewSinglePost.js
@@ -2,32 +2,40 @@ import React, { useEffect, useState, useContext } from "react";
 import Page from "./Page";
 import Axios from "axios";
 import LoadingDotsIcon from "./LoadingDotsIcon";
-import StateContext from "../StateContext";
+
 import { useParams, Link } from "react-router-dom";
+
 function ViewSinglePost() {
-    const appState = useContext( StateContext );
-    const { id } = useParams();
-    const [ isLoading, setIsLoading ] = useState( true );
-    const [ post, setPost ] = useState( [] );
-    useEffect( () => {
-        async function fetchPost() {
-            try {
-                const response = await Axios.get( `/post/${id}` );
-                console.log( response.data );
-                setIsLoading( false );
-                setPost( response.data );
-            } catch ( error ) {
-                console.log( "There was a problem.", error );
-            }
-        }
-        fetchPost();
-    }, [id] );
-    
-                if ( isLoading ) return <LoadingDotsIcon />;
+  const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    async function fetchPost() {
+      try {
+        const response = await Axios.get(`/post/${id}`);
+        console.log(response.data);
+        setIsLoading(false);
+        setPost(response.data);
+      } catch (error) {
+        console.log("There was a problem.", error);
+      }
+    }
+    fetchPost();
+  }, [id]);
+  function formatedDate() {
+    const date = new Date(post.createdDate);
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  }
+  if (isLoading)
+    return (
+      <Page title="...">
+        <LoadingDotsIcon />
+      </Page>
+    );
   return (
-      <Page title={post.title }>
+    <Page title={post.title}>
       <div className="d-flex justify-content-between">
-        <h2>Example Post Title</h2>
+        <h2>{post.title}</h2>
         <span className="pt-2">
           <a href="#" className="text-primary mr-2" title="Edit">
             <i className="fas fa-edit"></i>
@@ -40,31 +48,12 @@ function ViewSinglePost() {
 
       <p className="text-muted small mb-4">
         <a href="#">
-          <img
-            className="avatar-tiny"
-    src={appState.user.avatar}
-            alt="avatar"
-          />
+          <img className="avatar-tiny" src={post.author.avatar} alt="avatar" />
         </a>
-        Posted by <Link to="#">{appState.user.username}</Link> on {post.createdDate}
+        Posted by <Link to="#">{post.author.username}</Link> on {formatedDate()}
       </p>
 
-      <div className="body-content">
-        <p>
-          Lorem ipsum dolor sit <strong>example</strong> post adipisicing elit.
-          Iure ea at esse, tempore qui possimus soluta impedit natus voluptate,
-          sapiente saepe modi est pariatur. Aut voluptatibus aspernatur fugiat
-          asperiores at.
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quod
-          asperiores corrupti omnis qui, placeat neque modi, dignissimos, ab
-          exercitationem eligendi culpa explicabo nulla tempora rem? Lorem ipsum
-          dolor sit amet consectetur adipisicing elit. Iure ea at esse, tempore
-          qui possimus soluta impedit natus voluptate, sapiente saepe modi est
-          pariatur. Aut voluptatibus aspernatur fugiat asperiores at.
-        </p>
-      </div>
+      <div className="body-content">{post.body}</div>
     </Page>
   );
 }
