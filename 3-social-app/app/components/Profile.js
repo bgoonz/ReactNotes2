410cdaa40a0dd22f3bc285eaaeb6c643ccb016c2
Bryan C Guner <bryan.guner@gmail.com>
2023-05-24 10:54:37 -0400

    Profile.js useEffect to load follower count

--- 3-social-app/app/components/Profile.js
+++ 3-social-app/app/components/Profile.js
@@ -1,14 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Axios from "axios";
 import Page from "./Page";
+import StateContext from "../StateContext";
 function Profile() {
+  const { username } = useParams();
+  const appState = useContext(StateContext);
+  const [profileData, setProfileData] = useState({
+    profileUsername: "...",
+    profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
+    isFollowing: false,
+    counts: { postCount: "", followerCount: "", followingCount: "" },
+  });
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await Axios.post(`/profile/${username}`, {
+          token: appState.user.token,
+        });
+        console.log(response.data);
+        setProfileData(response.data);
+      } catch (e) {
+        console.log("There was a problem.");
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <Page title="Profile Screen">
       <h2>
-        <img
-          className="avatar-small"
-          src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-        />{" "}
-        brad
+        <img className="avatar-small" src={profileData.profileAvatar} />{" "}
+        {profileData.profileUsername}
         <button className="btn btn-primary btn-sm ml-2">
           Follow <i className="fas fa-user-plus"></i>
         </button>
@@ -16,38 +38,29 @@ function Profile() {
 
       <div className="profile-nav nav nav-tabs pt-2 mb-4">
         <a href="#" className="active nav-item nav-link">
-          Posts: 3
+          Posts: {profileData.counts.postCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Followers: 101
+          Followers: {profileData.counts.followerCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Following: 40
+          Following: {profileData.counts.followingCount}
         </a>
       </div>
 
       <div className="list-group">
         <a href="#" className="list-group-item list-group-item-action">
-          <img
-            className="avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />{" "}
+          <img className="avatar-tiny" src={appState.user.avatar} />{" "}
           <strong>Example Post #1</strong>
           <span className="text-muted small">on 2/10/2020 </span>
         </a>
         <a href="#" className="list-group-item list-group-item-action">
-          <img
-            className="avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />{" "}
+          <img className="avatar-tiny" src={appState.user.avatar} />{" "}
           <strong>Example Post #2</strong>
           <span className="text-muted small">on 2/10/2020 </span>
         </a>
         <a href="#" className="list-group-item list-group-item-action">
-          <img
-            className="avatar-tiny"
-            src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-          />{" "}
+          <img className="avatar-tiny" src={appState.user.avatar} />{" "}
           <strong>Example Post #3</strong>
           <span className="text-muted small">on 2/10/2020 </span>
         </a>
