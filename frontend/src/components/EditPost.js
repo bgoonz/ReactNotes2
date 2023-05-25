05e7198c50c8fb72e9490e43d7e1d32f43e1db3d
Bryan C Guner <bryan.guner@gmail.com>
2023-05-25 11:30:37 -0400

    edit post broken ...stuck in loading state

--- frontend/src/components/EditPost.js
+++ frontend/src/components/EditPost.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import useImmerReducer from "use-immer";
+import { useImmerReducer } from "use-immer";
 import Page from "./Page";
 import Axios from "axios";
 import LoadingDotsIcon from "./LoadingDotsIcon";
@@ -7,41 +7,46 @@ import LoadingDotsIcon from "./LoadingDotsIcon";
 import { useParams } from "react-router-dom";
 
 function EditPost() {
-    
-    const initialState = {
-        title: {
-            value: "",
-            hasErrors: false,
-            message: ""
-        },
-        body: {
-            value: "",
-            hasErrors: false,
-            message: ""
-        },
-        isFetching: true,
-        isSaving: false,
-        id: useParams().id,
-        sendCount: 0
+  const initialState = {
+    title: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    body: {
+      value: "",
+      hasErrors: false,
+      message: ""
+    },
+    isFetching: true,
+    isSaving: false,
+    id: useParams().id,
+    sendCount: 0
+  };
+  function ourReducer(draft, action) {
+    switch (action.type) {
+      case "fetchComplete":
+        draft.title.value = action.value.title;
+        draft.body.value = action.value.body;
+        draft.isfetching = false;
+        return;
+
+      default:
+        return draft;
     }
-    function ourReducer() {
-     
- }
-                
-    const [state,dispatch]=useImmerReducer(ourReducer,initialState);
-  const { id } = useParams();
-  const [isLoading, setIsLoading] = useState(true);
-  const [post, setPost] = useState([]);
+  }
+
+  const [state, dispatch] = useImmerReducer(ourReducer, initialState);
+
   useEffect(() => {
     const ourRequest = Axios.CancelToken.source();
     async function fetchPost() {
       try {
-        const response = await Axios.get(`/post/${id}`, {
+        const response = await Axios.get(`/post/${state.id}`, {
           cancelToken: ourRequest.token
         });
 
-        setIsLoading(false);
-        setPost(response.data);
+        dispatch({ type: "fetchComplete", value: response.data });
       } catch (error) {
         console.log("There was a problem, or the request was canceled", error);
       }
@@ -52,11 +57,11 @@ function EditPost() {
       // cleanup
       ourRequest.cancel();
     };
-  }, [id]);
+  }, [state.id, dispatch]);
 
-  if (isLoading)
+  if (state.isFetching)
     return (
-      <Page title="...">
+      <Page title="... Loading">
         <LoadingDotsIcon />
       </Page>
     );
@@ -67,14 +72,14 @@ function EditPost() {
           <label htmlFor="post-title" className="text-muted mb-1">
             <small>Title</small>
           </label>
-          <input autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" value={post.title} />
+          <input autoFocus name="title" id="post-title" className="form-control form-control-lg form-control-title" type="text" placeholder="" autoComplete="off" value={state.value.title} />
         </div>
 
         <div className="form-group">
           <label htmlFor="post-body" className="text-muted mb-1 d-block">
             <small>Body Content</small>
           </label>
-          <textarea name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={post.body} />
+          <textarea name="body" id="post-body" className="body-content tall-textarea form-control" type="text" value={state.value.body} />
         </div>
 
         <button className="btn btn-primary">Save Updates</button>
