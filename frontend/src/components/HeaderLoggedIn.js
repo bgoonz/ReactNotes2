1ac7a4eb3856e3475a890190e61b5492a4ea5539
Bryan C Guner <bryan.guner@gmail.com>
2023-05-31 17:15:12 -0400

    tooltip for search chat and profile working with updated react tooltip syntax

--- frontend/src/components/HeaderLoggedIn.js
+++ frontend/src/components/HeaderLoggedIn.js
@@ -3,6 +3,7 @@ import React, { useContext } from "react";
 import DispatchContext from "../DispatchContext";
 import StateContext from "../StateContext";
 import { Link } from "react-router-dom";
+import { Tooltip as ReactTooltip } from "react-tooltip";
 function HeaderLoggedIn(props) {
   //app means appwide or global
   const appDispatch = useContext(DispatchContext);
@@ -18,24 +19,19 @@ function HeaderLoggedIn(props) {
 
   return (
     <div className="flex-row my-3 my-md-0">
-      <a
-        onClick={handleSearch}
-        href="#"
-        className="text-white mr-2 header-search-icon"
-      >
+      <a onClick={handleSearch} href="#" data-tooltip-id="search" data-tooltip-content="Search" className="text-white mr-2 header-search-icon">
         <i className="fas fa-search"></i>
       </a>
-      <span className="mr-2 header-chat-icon text-white">
+      <ReactTooltip place="bottom" id="search" className="custom-tooltip" />
+      <span data-tooltip-id="chat" data-tooltip-content="Chat" className="mr-2 header-chat-icon text-white">
         <i className="fas fa-comment"></i>
         <span className="chat-count-badge text-white"> </span>
       </span>
+      <ReactTooltip place="bottom" id="chat" className="custom-tooltip" />
       <Link to={`/profile/${appState.user.username}`} className="mr-2">
-        <img
-          className="small-header-avatar"
-          alt="avatar"
-          src={appState.user.avatar}
-        />
+        <img data-tooltip-id="profile" data-tooltip-content="Profile" className="small-header-avatar" alt="avatar" src={appState.user.avatar} />
       </Link>
+      <ReactTooltip place="bottom" id="profile" className="custom-tooltip" />
       <Link className="btn btn-sm btn-success mr-2" to="/create-post">
         Create Post
       </Link>
