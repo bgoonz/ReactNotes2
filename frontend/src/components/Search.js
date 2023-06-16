5be8c68cb70f2c1e6bfe5561f28a4a0711a3d73c
Bryan C Guner <bryan.guner@gmail.com>
2023-06-16 14:44:30 -0400

    search bar fully functional

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/anchor-is-valid */
@@ -12,7 +13,7 @@ function Search() {
     searchTerm: "",
     results: [],
     show: "neither",
-    requestCount: 0,
+    requestCount: 0
   });
   function handleSearchClose(event) {
     event.preventDefault();
@@ -27,32 +28,40 @@ function Search() {
 
   //Search Debounce useEffect
   useEffect(() => {
-    const delay = setTimeout(() => {
+    if (state.searchTerm.trim()) {
       setState((draft) => {
-        draft.requestCount++;
+        draft.show = "loading";
       });
-    }, 1000);
-    return () => clearTimeout(delay);
+      const delay = setTimeout(() => {
+        setState((draft) => {
+          draft.requestCount++;
+        });
+      }, 1000);
+      return () => clearTimeout(delay);
+    } else {
+      setState((draft) => {
+        draft.show = "neither";
+      });
+    }
   }, [state.searchTerm]);
 
   useEffect(() => {
     if (state.requestCount) {
-        const ourRequest = Axios.CancelToken.source();
-        async function fetchResults() { 
-            try {
-                const response = await Axios.post( '/search', { searchTerm: state.searchTerm }, { cancelToken: ourRequest.token } );
-                console.log(response.data)
-                setState( ( draft ) => {
-                    draft.results = response.data;
-                    draft.show = "results";
-                } );
-                
-            } catch ( error ) {
-                console.log(error)
-            }
+      const ourRequest = Axios.CancelToken.source();
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/search", { searchTerm: state.searchTerm }, { cancelToken: ourRequest.token });
+          console.log(response.data);
+          setState((draft) => {
+            draft.results = response.data;
+            draft.show = "results";
+          });
+        } catch (error) {
+          console.log(error);
         }
-        fetchResults();
-        return () => ourRequest.cancel();
+      }
+      fetchResults();
+      return () => ourRequest.cancel();
     }
   }, [state.requestCount]);
 
@@ -75,15 +84,7 @@ function Search() {
           <label htmlFor="live-search-field" className="search-overlay-icon">
             <i className="fas fa-search"></i>
           </label>
-          <input
-            autoFocus
-            type="text"
-            autoComplete="off"
-            id="live-search-field"
-            className="live-search-field"
-            placeholder="What are you interested in?"
-            onChange={handleInput}
-          />
+          <input autoFocus type="text" autoComplete="off" id="live-search-field" className="live-search-field" placeholder="What are you interested in?" onChange={handleInput} />
           <span onClick={handleSearchClose} className="close-live-search">
             <i className="fas fa-times-circle"></i>
           </span>
@@ -92,40 +93,24 @@ function Search() {
 
       <div className="search-overlay-bottom">
         <div className="container container--narrow py-3">
-          <div className="live-search-results live-search-results--visible">
+          <div className={"circle-loader " + (state.show === "loading" ? "circle-loader--visible" : "")}></div>
+          <div className={"live-search-results " + (state.show === "results" ? "live-search-results--visible" : "")}>
             <div className="list-group shadow-sm">
               <div className="list-group-item active">
-                <strong>Search Results</strong> (3 items found)
+                <strong>Search Results</strong> ({state.results.length} {state.results.length > 1 ? "items" : "item"} found)
               </div>
-              <a href="#" className="list-group-item list-group-item-action">
-                <img
-                  className="avatar-tiny"
-                  src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-                  alt="gravitar"
-                />{" "}
-                <strong>Example Post #1</strong>
-                <span className="text-muted small">by brad on 2/10/2020 </span>
-              </a>
-              <a href="#" className="list-group-item list-group-item-action">
-                <img
-                  className="avatar-tiny"
-                  src="https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128"
-                  alt="gravitar"
-                />{" "}
-                <strong>Example Post #2</strong>
-                <span className="text-muted small">
-                  by barksalot on 2/10/2020{" "}
-                </span>
-              </a>
-              <a href="#" className="list-group-item list-group-item-action">
-                <img
-                  className="avatar-tiny"
-                  src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128"
-                  alt="gravitar"
-                />{" "}
-                <strong>Example Post #3</strong>
-                <span className="text-muted small">by brad on 2/10/2020 </span>
-              </a>
+              {state.results.map((post) => {
+                const date = new Date(post.createdDate);
+                const formattedDate = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+                return (
+                  <a href={`/post/${post._id}`} className="list-group-item list-group-item-action" key={post._id}>
+                    <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong>{" "}
+                    <span className="text-muted small">
+                      {!post.isVisitorOwner && `by ${post.author.username}`} on {formattedDate}{" "}
+                    </span>
+                  </a>
+                );
+              })}
             </div>
           </div>
         </div>
